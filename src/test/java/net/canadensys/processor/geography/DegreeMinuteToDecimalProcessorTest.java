5b15f219a34d960f3f12ae4cfcdb529ebbe38f9a
cgendreau <christiangendreau@gmail.com>
2013-04-10 13:44:38 -0400

    updated DegreeMinuteToDecimalProcessorTest to use a test file(DDMMSSCoordinatesFormat.txt)

--- src/test/java/net/canadensys/processor/geography/DegreeMinuteToDecimalProcessorTest.java
+++ src/test/java/net/canadensys/processor/geography/DegreeMinuteToDecimalProcessorTest.java
@@ -4,10 +4,16 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.net.URISyntaxException;
+
+import net.canadensys.FileBasedTest;
 import net.canadensys.processor.AbstractDataProcessor;
 import net.canadensys.processor.ProcessingResult;
 import net.canadensys.processor.dwc.mock.MockOccurrenceModel;
 import net.canadensys.processor.dwc.mock.MockRawOccurrenceModel;
+import net.canadensys.utils.NumberUtils;
 
 import org.junit.Test;
 
@@ -18,30 +24,42 @@ import org.junit.Test;
  */
 public class DegreeMinuteToDecimalProcessorTest {
 	
+	private static final String TEST_FILE_NAME = "/DDMMSSCoordinatesFormat.txt";
+	
 	private int LAT_IDX = 0;
 	private int LNG_IDX = 1;
 	
 	@Test
 	public void testDegreeMinuteToDecimalProcessor(){
-		DegreeMinuteToDecimalProcessor dmtdProcessor = new DegreeMinuteToDecimalProcessor();
+		final DegreeMinuteToDecimalProcessor dmtdProcessor = new DegreeMinuteToDecimalProcessor();
 		Double[] output = new Double[2];
-		//test supported syntax
-		output = dmtdProcessor.process("40°26′47″N","74° 0' 21.5022\"W", null);
-		assertEquals(40.44639f,output[LAT_IDX].floatValue(),0);
-		output = dmtdProcessor.process("40:26:47N ","30°17′12″E", null);
-		assertEquals(40.44639f,output[LAT_IDX].floatValue(),0);
-		output = dmtdProcessor.process("40°26'47\"N","30°17′12″E", null);
-		assertEquals(40.44639f,output[LAT_IDX].floatValue(),0);
-		output = dmtdProcessor.process("40d 26' 47\" N","30°17′12″E", null);
-		assertEquals(40.44639f,output[LAT_IDX].floatValue(),0);
-		output = dmtdProcessor.process("40d 26m 47N ","30°17′12″E", null);
-		assertEquals(40.44639f,output[LAT_IDX].floatValue(),0);
-		output = dmtdProcessor.process("40d 26m 47sN ","30°17′12″E", null);
-		assertEquals(40.44639f,output[LAT_IDX].floatValue(),0);
-		output = dmtdProcessor.process("40d 26m 47sN","74° 0' 21.5022\"W",null);
-		assertEquals(-74.0059731f,output[LNG_IDX].floatValue(),0);
 		
-		//Math.abs(expected - actual) < epsilon
+		//test supported syntax (from the test file)
+		try {
+			final File dateFile = new File(getClass().getResource(TEST_FILE_NAME).toURI());
+			FileBasedTest fileBasedTest = new FileBasedTest(dateFile) {
+				Double[] output = null;
+				Double lat, lng, delta;
+				@Override
+				public void processLine(String[] elements, int lineNumber) {
+					if(elements.length == 5){
+						output = dmtdProcessor.process(elements[0], elements[1], null);
+						lat = NumberUtils.parseNumber(elements[2], Double.class);
+						lng = NumberUtils.parseNumber(elements[3], Double.class);
+						delta = NumberUtils.parseNumber(elements[4], Double.class);
+						
+						String assertText = "[Line #" + lineNumber + " in " + dateFile.getName()+"]";
+						assertEquals(assertText, lat, output[LAT_IDX], delta);
+						assertEquals(assertText, lng, output[LNG_IDX], delta);
+					}
+				}
+			};
+			
+			fileBasedTest.processFile();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		
 		output = dmtdProcessor.process("40°26′47″N","74° 0' 21.5022\"W", null);
 		assertEquals(40.44639,output[LAT_IDX],0.00001);
 		
@@ -76,8 +94,9 @@ public class DegreeMinuteToDecimalProcessorTest {
 		MockOccurrenceModel occModel = new MockOccurrenceModel();
 		rawModel.setVerbatimLatitude("40°26′47″N");
 		rawModel.setVerbatimLongitude("30°17′12″E");
-		dmtdProcessor = new DegreeMinuteToDecimalProcessor("verbatimLatitude","verbatimLongitude","decimalLatitude", "decimalLongitude");
-		dmtdProcessor.processBean(rawModel, occModel, null, null);
+
+		DegreeMinuteToDecimalProcessor dmtdProcessor2 = new DegreeMinuteToDecimalProcessor("verbatimLatitude","verbatimLongitude","decimalLatitude", "decimalLongitude");
+		dmtdProcessor2.processBean(rawModel, occModel, null, null);
 		assertEquals(40.44639f, occModel.getDecimalLatitude().floatValue(),0);
 	}
 	
